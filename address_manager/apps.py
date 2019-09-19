374bec6890542618c9d1b9dd5cb5b09b8982f223
Umair Ashraf <umrashrf@gmail.com>
2019-09-19 12:39:34 -0400

    added inital django cms project with django scraper

---
+++ address_manager/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AddressManagerConfig(AppConfig):
+    name = 'address_manager'
---
+++ address_manager/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from dynamic_scraper.models import Scraper, SchedulerRuntime
+from scrapy_djangoitem import DjangoItem
+
+
+class AddressUnitList(models.Model):
+    choice = models.CharField(max_length=150)
+
+
+class AddressWebsite(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+    scraper = models.ForeignKey(Scraper, blank=True, null=True, on_delete=models.SET_NULL)
+    scraper_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Address(models.Model):
+    street_number = models.CharField(max_length=500)
+    address1 = models.CharField(max_length=100)
+    address2 = models.CharField(max_length=100, blank=True)
+    unit_type = models.ForeignKey(AddressUnitList, on_delete=models.CASCADE, blank=False)
+    unit_number = models.CharField(max_length=500, blank=True)
+    city = models.CharField(max_length=250)
+    state = models.CharField(max_length=250)
+    country = models.CharField(max_length=250)
+    zipcode = models.CharField(max_length=50)
+    source = models.ForeignKey(AddressWebsite, on_delete=models.CASCADE)
+    checker_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
+    note = models.TextField(blank=True)
+    
+    def __unicode__(self):
+        return self.title
+
+    def pre_delete(sender, instance, using):
+        if isinstance(instance, Address):
+            if instance.checker_runtime:
+                instance.checker_runtime.delete()
+
+class AddressItem(DjangoItem):
+    django_model = Address
