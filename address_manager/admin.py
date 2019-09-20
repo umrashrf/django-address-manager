a145e20f2e71b715aac80d27d0373de3b3026ed3
Umair Ashraf <umrashrf@gmail.com>
2019-09-20 13:47:32 -0400

    added django admin for custom models

--- address_manager/admin.py
+++ address_manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AddressUnitList, AddressWebsite, Address
+
+admin.site.register(AddressUnitList)
+admin.site.register(AddressWebsite)
+admin.site.register(Address)
